a6b8bbd6999acf514614433317a841e7bdbcf309
Zhimin Xiang <zhiminx@google.com>
2020-09-22 10:35:40 -0700

    Pin deps to release-0.18 (#341)

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -27,7 +27,7 @@ export GO111MODULE=on
 export GOFLAGS=-mod=vendor
 
 # This controls the release branch we track.
-VERSION="master"
+VERSION="release-0.18"
 
 # The list of dependencies that we track at HEAD and periodically
 # float forward in this repository.
