bd32ea76811a40366f32df113d2e4792c0511df0
Dave Protasowski <dprotaso@gmail.com>
2022-07-04 09:17:45 -0400

    generate schema for caching types (#655)

--- hack/verify-codegen.sh
+++ hack/verify-codegen.sh
@@ -19,7 +19,7 @@ set -o nounset
 set -o pipefail
 
 readonly REPO_ROOT_DIR="$(git rev-parse --show-toplevel)"
-readonly TMP_DIFFROOT="$(mktemp -d -p ${REPO_ROOT_DIR})"
+readonly TMP_DIFFROOT="$(mktemp -d ${REPO_ROOT_DIR}/tmpdiffroot.XXXXXX)"
 
 cleanup() {
   rm -rf "${TMP_DIFFROOT}"
@@ -33,16 +33,20 @@ cleanup
 mkdir -p "${TMP_DIFFROOT}/pkg"
 cp -aR "${REPO_ROOT_DIR}/go.sum" "${REPO_ROOT_DIR}/pkg" "${REPO_ROOT_DIR}/vendor" "${TMP_DIFFROOT}"
 
+mkdir -p "${TMP_DIFFROOT}/config"
+cp -a "${REPO_ROOT_DIR}/config"/* "${TMP_DIFFROOT}/config"
+
 # TODO(mattmoor): We should be able to rm -rf pkg/client/ and vendor/
 
 "${REPO_ROOT_DIR}/hack/update-codegen.sh"
 echo "Diffing ${REPO_ROOT_DIR} against freshly generated codegen"
 ret=0
-diff -Naupr "${REPO_ROOT_DIR}/pkg" "${TMP_DIFFROOT}/pkg" || ret=1
-diff -Naupr --no-dereference "${REPO_ROOT_DIR}/vendor" "${TMP_DIFFROOT}/vendor" || ret=1
+diff -Nupr --no-dereference "${REPO_ROOT_DIR}/config" "${TMP_DIFFROOT}/config" || ret=1
+diff -Nupr --no-dereference "${REPO_ROOT_DIR}/pkg" "${TMP_DIFFROOT}/pkg" || ret=1
+diff -Nupr --no-dereference "${REPO_ROOT_DIR}/vendor" "${TMP_DIFFROOT}/vendor" || ret=1
 
 # Restore working tree state
-rm -fr "${TMP_DIFFROOT}/config"
+rm -fr "${REPO_ROOT_DIR}/config"
 rm -fr "${REPO_ROOT_DIR}/go.sum" "${REPO_ROOT_DIR}/pkg" "${REPO_ROOT_DIR}/vendor"
 cp -aR "${TMP_DIFFROOT}"/* "${REPO_ROOT_DIR}"
 
